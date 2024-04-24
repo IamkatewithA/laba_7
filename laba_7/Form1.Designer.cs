79a47ad6a914fefa109d8909b67be57f0af92b0b
Home <Home@FLTP-5i5-8256>
2024-04-24 04:15:17 +0300

    Добавьте файлы проекта.

---
+++ laba_7/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace laba_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(352, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 34);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(352, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Расставить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(439, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "мин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(145, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(538, 355);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Игра сапер";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ laba_7/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace laba_7
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer timer;
+        int m, s;
+
+        int height = 5; // size of the field
+        int distance = 40; // didtance between buttons
+        ButtonExtended[,] allButtons; // array of buttons
+        int cellsOpened = 0; //counter of oppend cells
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            allButtons = new ButtonExtended[height, height];
+            Random rng = new Random();
+            for(int x = 40; (x - 40) < height * distance; x += distance)
+            {
+                for (int y = 40; (y - 40) < height * distance; y += distance)
+                {
+                    ButtonExtended button = new ButtonExtended();
+                    button.Location = new Point(x, y);
+                    button.Size = new Size(35, 35);
+                    button.isClickable = true;
+
+                    allButtons[(x - 40)/distance, (y - 40) / distance] = button;
+
+                    Controls.Add(button);
+                    button.MouseUp += new MouseEventHandler(FieldClick);
+                }
+            }
+
+
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += OnTimeEvent;
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                s++;
+                if (s == 60)
+                {
+                    s = 0;
+                    m++;
+                }
+            }));
+            label2.Text = string.Format("{0}:{1}", m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+        }
+
+        void FieldClick(object sender, MouseEventArgs e)
+        {
+            ButtonExtended button = (ButtonExtended)sender;
+            if (e.Button == MouseButtons.Left && button.isClickable)
+            {
+                if (button.isBomb)
+                {
+                    Explode(button);
+                }
+                else
+                {
+                    EmptyFieldClick(button);
+                    cellsOpened++;
+                    
+                }
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                
+                button.isClickable = !button.isClickable;
+                if (!button.isClickable)
+                {
+                    button.Text = "B";
+                }
+                else
+                {
+                    button.Text = "";
+                }
+            }
+            CheckWin();
+        }
+        void Explode(ButtonExtended button)
+        {
+            timer.Stop();
+            for (int x = 0; x < height; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (allButtons[x, y].isBomb)
+                    {
+                        allButtons[x, y].Text = "*";
+                    }
+                }
+            }
+            MessageBox.Show("Вы проиграли");
+            
+        }
+        void EmptyFieldClick(ButtonExtended button)
+        {
+            for (int x = 0; x < height; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (allButtons[x, y] == button)
+                    {
+                        button.Text = "" + CountBombsAround(x, y);
+                    }
+                }
+            }
+            button.Enabled = false;
+        }
+        int CountBombsAround (int xB, int yB)
+        {
+            int bombCount = 0;
+            for (int x = xB - 1; x <= xB + 1; x++)
+            {
+                for (int y = yB - 1; y <= yB + 1; y++)
+                {
+                    if(x >= 0 && x < height && y >= 0 && y < height)
+                    {
+                        if (allButtons[x, y].isBomb)
+                        {
+                            bombCount++;
+                        }
+                    }
+                  
+                }
+            }
+            return bombCount;
+        }
+        int i = 0;
+        private void button1_Click(object sender, EventArgs e) // ЗАПИХНУТЬ В ОТДЕЛЬНЫЙ КЛАСС
+        {
+            try
+            {
+                string bomb = textBox1.Text;
+                int bMax = int.Parse(bomb);
+                Random random = new Random();
+                int bCounter = 0;
+                while (bCounter < bMax)
+                {
+                    int randx = random.Next(0, height);
+                    int randy = random.Next(0, height);
+                    if (!allButtons[randx, randy].isBomb)
+                    {
+                        allButtons[randx, randy].isBomb = true;
+                        bCounter++;
+                    }
+                }
+                MessageBox.Show("Бомбы расставлены\nПриступайте к игре");
+                timer.Start();
+            }
+            catch
+            {
+                MessageBox.Show("Введите число от 1 до 10");
+            }
+        }
+    void CheckWin()
+        {
+            int cells = height * height;
+            int emptyCells = cells - int.Parse(textBox1.Text);
+            if (cellsOpened == emptyCells)
+            {
+                timer.Stop();
+                MessageBox.Show("Вы победили");
+            }
+        }
+
+
+    }
+    class ButtonExtended:Button
+    {
+        public bool isBomb;
+        public bool isClickable;
+    }
+}
